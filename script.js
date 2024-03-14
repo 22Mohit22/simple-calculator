060926bf65de65dee7bfa3da9a89197efee48255
Mohit pandey <mohit20020422@gmail.com>
2024-03-14 19:03:31 +0530

    added functionality to handle num clicks

--- script.js
+++ script.js
@@ -6,3 +6,58 @@ const displayNum2 = document.querySelector('#display-num2');
 const equalsEl = document.querySelector('#equals-to');
 const clearEl = document.querySelector('#clear');
 const decimalEl = document.querySelector('#decimal');
+
+function add() {
+    return parseFloat(x) + parseFloat(y);
+}
+function sub() {
+    return parseFloat(x) - parseFloat(y);
+}
+function mul() {
+    return parseFloat(x) * parseFloat(y);
+}
+function div() {
+    return parseFloat(x) / parseFloat(y);
+}
+
+function calculate() {
+    if(operand == '+') {
+        return add();
+    } else if (operand == '-') {
+        return sub();
+    } else if (operand == 'x') {
+        return mul();
+    } else if (operand == '/') {
+        return div();
+    } else {
+        console.log('something went wrong');
+    }
+}
+
+let x = '';
+let operand = '';
+let y = ''
+
+allNumBtns.forEach(number => {
+    number.addEventListener('click', () => {
+        handleNumClick(number.textContent);
+    })
+})
+
+function handleNumClick(e) {
+
+    // this func checks if x is empty or x is not empty 
+    // and operator is empty to then it will append 
+    // the value to the x variable and to display 
+    // if both the condition are false then it appends it
+    // to y variable
+
+
+    if(x == '' || (x != '' && operand == '')) {
+        displayNum1.value += e;
+        x += e;
+    } else {
+        displayNum2.value += e;
+        y += e;
+    }
+}
